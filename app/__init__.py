2263fa895f3def45657229422fae7a1e7775f647
ttime1100 <ttime1100@gmail.com>
2018-04-11 06:14:49 -0700

    new files

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from app.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_dropzone import Dropzone
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+
+login.login_view = 'login'
+
+mail = Mail(app)
+
+bootstrap = Bootstrap(app)
+
+moment = Moment(app)
+
+dropzone = Dropzone(app)
+
+from app import routes, models
