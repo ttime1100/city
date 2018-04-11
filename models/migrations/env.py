d97c05c3e09e46eeb5a5c62de92386ad349faa55
ttime1100 <ttime1100@gmail.com>
2018-04-11 10:30:27 -0700

    new

--- models/migrations/env.py
+++ models/migrations/env.py
@@ -15,7 +15,7 @@ logger = logging.getLogger('alembic.env')
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from app import app.models
+# from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from flask import current_app
 config.set_main_option('sqlalchemy.url',
